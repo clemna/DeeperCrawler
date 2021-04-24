6c14788c8dc665334d24528980a664d4f151655c
LastWitcher <67241030+LastWitcher117@users.noreply.github.com>
2021-04-24 23:39:08 +0200

    New Scenes
    [+] Main Menu
    [+] Test Scene
    [+] Menu Script

---
+++ DeeperCrawler/Assets/Scripts/Main_Menu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu : MonoBehaviour
+{
+    public Canvas credits;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("TestScene");
+    }
+
+    public void Credits()
+    {
+        credits.enabled = true;
+    }
+
+    public void CreditsBackButton()
+    {
+        credits.enabled = false;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("WYSZED£EM KURWA");
+    }
+}
+
